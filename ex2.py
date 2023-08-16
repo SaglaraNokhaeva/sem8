d3a9b5f50fa98a53277ea43ef1b74f4676bebb65
Saglara <nohaevass@yandex.ru>
2023-08-16 22:19:11 +0300

    1

--- ex2.py
+++ ex2.py
@@ -10,3 +10,30 @@
 # от уровня доступа.
 # При перезапуске функции уже записанные в файл данные
 # должны сохраняться.
+import json
+
+
+def func():
+    while True:
+        str_inp = input('Введите данные: ')
+        if str_inp:
+            name, pers_id, level = str_inp.split()
+            level = int(level)
+            if not 0 < level < 8:
+                print('Давай сначала')
+                continue
+            with open('my_file.json', 'r') as f:
+                try:
+                    data = json.load(f)
+                except:
+                    data = {}
+            if level not in data:
+                data[level] = {}
+            data[level][pers_id] = name
+            with open('my_file.json', 'w') as f:
+                json.dump(data, f)
+        else:
+            break
+
+
+func()
