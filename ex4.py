7f18d82c0e53f4950e9bffebef88497d9dc500f5
Saglara <nohaevass@yandex.ru>
2023-08-17 14:40:57 +0300

    1

--- ex4.py
+++ ex4.py
@@ -8,4 +8,23 @@
 # csv файла представлена как отдельный json словарь.
 # Имя исходного и конечного файлов передавайте как аргументы
 # функции.
+import csv
+import json
 
+
+def func(source_file, dest_file):
+
+
+    with (open(source_file, 'r') as f1,
+          open(dest_file, 'w', encoding='utf-8') as file):
+        csv_file = csv.reader(f1, delimiter=';')
+        next(csv_file)
+        new_dict = {}
+        for level, pers_id, name in csv_file:
+            pers_id = pers_id.rjust(10, '0')
+            name = name.title()
+            hash_id = hash(name + pers_id)
+            new_dict[hash_id] = {pers_id: [name, level]}
+        json.dump(new_dict, file, indent=2)
+
+func('name.csv', 'names.json')
