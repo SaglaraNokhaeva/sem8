eaf474bbbf5000d97e6d5ce5ccfd3e4656287cf8
Saglara <nohaevass@yandex.ru>
2023-08-17 22:41:33 +0300

    1

--- my_package/get_csv_file.py
+++ my_package/get_csv_file.py
@@ -7,7 +7,6 @@ def my_csv():
 
     with (open('result.json', 'r') as f1,
           open('result.csv', 'w', encoding='utf-8', newline='') as file):
-
         data = json.load(f1)
         print(data)
         columns = ['object', 'file or dir', 'size', 'parent_dir']
@@ -16,11 +15,4 @@ def my_csv():
         result = []
         for key, value in data.items():
             result.append([key, *data[key]])
-            # result.extend([value])
-            # result.append(value)
-            # for i in data[key]:
-            #     result.append(*i)
         writer.writerows(result)
-
-
-my_csv()
