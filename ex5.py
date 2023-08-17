8dd6c0188934ac576d2a80cb620f00b902de271e
Saglara <nohaevass@yandex.ru>
2023-08-17 15:23:30 +0300

    1

--- ex5.py
+++ ex5.py
@@ -2,4 +2,16 @@
 # Напишите функцию, которая ищет json файлы в указанной
 # директории и сохраняет их содержимое в виде
 # одноимённых pickle файлов.
+import json
+import os
+import pickle
 
+
+def rename_file():
+    for file in os.listdir():
+        if file.endswith('.json'):
+            with (open(file, 'r') as file_inp,
+                open(f'{file[:-5]}.pickle', 'wb') as file_out):
+                pickle.dump((json.load(file_inp)), file_out)
+
+rename_file()
