c9faf04b2236b0f3021c9091956ed9d9f7870e17
Saglara <nohaevass@yandex.ru>
2023-08-17 15:56:59 +0300

    1

--- ex6.py
+++ ex6.py
@@ -4,4 +4,19 @@
 # Для тестирования возьмите pickle версию файла из задачи 4 этого семинара.
 # Функция должна извлекать ключи словаря для заголовков
 # столбца из переданного файла.
+import pickle
 
+
+def csv_converter(file):
+    with (open(file, 'rb') as file_inp,
+        open(f'{file[:-7]}.csv', 'w') as file_out):
+        data = pickle.load(file_inp)
+        head = data.keys()
+        import csv
+        writer = csv.writer(file_out, delimiter=';')
+        writer.writerow(head)
+        for key, values in data.items():
+            a, b = tuple(*values.values())
+            writer.writerow([*(values.keys()), a, b])
+
+csv_converter('names.pickle')
